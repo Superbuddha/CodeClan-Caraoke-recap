9862c4e04c9f485835267e05b8d5702953ce04df
Stephan Constantial <Sconstantial@yahoo.com>
2018-09-10 15:08:02 +0100

    add guests to room functions

--- room.rb
+++ room.rb
@@ -1,8 +1,35 @@
 class Room
 
-  attr_reader :name
+  attr_reader :name, :songs, :guests
 
-  def initialize(name)
+  def initialize(name, songs,guests)
     @name = name
+    @songs = songs
+    @guests = []
   end
+
+  def number_of_songs()
+    return @songs.count()
+  end
+
+def number_of_guests
+  return @guests.count()
+end
+
+def check_in_guest(guest)
+  @guests.push(guest)
+end
+
+#how to write only one fucntion for the add
+def check_in_guests(guests)
+  for guest in guests
+    @guests.push(guest)
+  end
+  #or guests.each{|guest| check_in_guest(guest)}
+end
+
+def remove_guest(guest)
+  @guests.pop(guest)
+end
+
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -10,10 +10,37 @@ class TestRoom <MiniTest::Test
     @song1 =Song.new("Teenage Wasteland")
     @song2 = Song.new("Beastly")
     @song3 = Song.new("Time Bending")
-    @guest = Guest.new("Henry")
-    @room =Room.new("Indie Rock Room")
+    @songs = [@song1, @song2, @song3]
+    @guest1 = Guest.new("Henry")
+    @guest2 = Guest.new("Larry")
+    @guests = [@guest1, @guest2]
+    @room =Room.new("Indie Rock Room", @songs, @guests)
   end
+
   def test_room_has_name
-    assert_equal("Indie Rock Room", @room.name)
+    assert_equal("Indie Rock Room", @room.name())
+end
+
+def test_room_has_songs
+  assert_equal(3, @room.number_of_songs())
+end
+
+def test_room_starts_empty
+  assert_equal(0, @room.number_of_guests())
+end
+
+def test_room_can_add_guests
+  @room.check_in_guest(@guest1)
+  assert_equal(1, @room.number_of_guests())
+end
+
+def test_add_multiple_guests
+  @room.check_in_guests(@guests)
+  assert_equal(2, @room.number_of_guests())
+end
+
+def test_can_remove_guest
+  @room.remove_guest(@guest1)
+  assert_equal(1, @room.number_of_guests())
 end
 end
